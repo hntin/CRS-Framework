2ff17dade5168f3a5de11da714547b5024dae8d4
tin.huynh.uit@gmail.com <tin.huynh.uit@gmail.com@585e8c69-b0d5-62e4-9894-778d353b6c62>
2013-06-10 09:48:33 +0000

    Include existing classes into the CRS Framework

---
+++ trunk/CRSFramework/src/uit/tkorg/crs/method/JGibbLDA.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.crs.method;
+
+import java.io.File;
+import java.util.HashMap;
+import jgibblda.Estimator;
+import jgibblda.LDACmdOption;
+import uit.tkorg.utility.TextFileProcessor;
+
+/**
+ *
+ * @author tin
+ */
+public class JGibbLDA {
+    static StringBuffer buffInputFormatLDA = new StringBuffer();
+    static StringBuffer buffAuthorIDIndex = new StringBuffer();
+
+    public void process() {
+        LDACmdOption ldaCmdOption = new LDACmdOption();
+        ldaCmdOption.setEst(true);
+        ldaCmdOption.setEstc(false);
+        ldaCmdOption.setInf(false);
+        ldaCmdOption.setDir("C:\\CRS-Experiment\\Test\\OutStem\\New folder");
+        ldaCmdOption.setDfile("OutStem_ap1.dat");
+        ldaCmdOption.setAlpha(0.01);
+        ldaCmdOption.setBeta(0.01);
+        ldaCmdOption.setK(100);
+        ldaCmdOption.setNiters(1000);
+        ldaCmdOption.setSavestep(100);
+        ldaCmdOption.setTwords(10);
+
+        Estimator estimator = new Estimator();
+        estimator.init(ldaCmdOption);
+        estimator.estimate();
+    }
+
+    public void formatInputForLDA(String rootPath) {
+        File mainFolder = new File(rootPath); // C:\CRS-Experiment\OutStem
+        File[] subFolderList = mainFolder.listFiles();
+        int idx = 0;
+        for (int i = 0; i < subFolderList.length; i++) {
+            if (subFolderList[i].isDirectory()) {
+                File[] fList = subFolderList[i].listFiles();
+                for (int j = 0; j < fList.length; j++) {
+                    if (fList[j].isFile()) {
+                        // Doc va bo vao buffer
+                        buffInputFormatLDA.append(TextFileProcessor.readTextFile(fList[j].getAbsolutePath()));
+                        buffAuthorIDIndex.append(fList[j].getName() + "\t" + (idx++) + "\n");
+                    }
+                    buffInputFormatLDA.append("\n");
+                }
+            }
+        }
+
+        buffInputFormatLDA.insert(0, idx + "\n");
+
+        TextFileProcessor.writeTextFile(rootPath + "\\CRS-InputDataLDA.dat", buffInputFormatLDA.toString());
+        TextFileProcessor.writeTextFile(rootPath + "\\CRS-AuthorIDIdx.dat", buffAuthorIDIndex.toString());
+    }
+}
