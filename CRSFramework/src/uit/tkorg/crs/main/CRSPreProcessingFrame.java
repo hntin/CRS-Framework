38998887b5032f0884fee3c2e1efd16ed983e184
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-06-10 13:16:24 +0000

    Building CRS Frameworks GUI

--- CRSFramework/src/uit/tkorg/crs/main/CRSPreProcessingFrame.java
+++ CRSFramework/src/uit/tkorg/crs/main/CRSPreProcessingFrame.java
@@ -8,12 +8,12 @@ package uit.tkorg.crs.main;
  *
  * @author Tin Huynh
  */
-public class PreProcessingFrame extends javax.swing.JFrame {
+public class CRSPreProcessingFrame extends javax.swing.JFrame {
 
     /**
-     * Creates new form PreProcessingFrame
+     * Creates new form CRSPreProcessingFrame
      */
-    public PreProcessingFrame() {
+    public CRSPreProcessingFrame() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class PreProcessingFrame extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CRSPreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CRSPreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CRSPreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CRSPreProcessingFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PreProcessingFrame().setVisible(true);
+                new CRSPreProcessingFrame().setVisible(true);
             }
         });
     }
