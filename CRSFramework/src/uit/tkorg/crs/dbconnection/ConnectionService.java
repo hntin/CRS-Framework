614b9c76dc1c37b6aac8e1be4a93e49d31ada172
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-06-13 07:14:17 +0000

    Building CRS Frameworks GUI

--- CRSFramework/src/uit/tkorg/crs/dbconnection/ConnectionService.java
+++ CRSFramework/src/uit/tkorg/crs/dbconnection/ConnectionService.java
@@ -16,11 +16,10 @@ import javax.sql.DataSource;
  * @author TinHuynh
  */
 public class ConnectionService {
-    
-     /**
-     * loadJDBCDriver
-     * @throws Exception 
-     */
+    public static String dbURL;
+    public static String dbUserName;
+    public static String dbPassword;
+            
     protected static void loadJDBCDriver() throws Exception {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -29,30 +28,14 @@ public class ConnectionService {
         }
     }
 
-    /**
-     * Return the connection.
-     *
-     * @return Connection to the database
-     * @throws NamingException
-     * @throws SQLException
-     */
     public static Connection getConnection() throws Exception {
         Connection connect = null;
         if (connect == null) {
             loadJDBCDriver();
-            String jdbcutf8 = "&useUnicode=true&characterEncoding=UTF-8";
-            String url = "jdbc:mysql://" + "localhost"
-                    + ":" + "3306"
-                    + "/" + "MAS"
-                    + "?user=" + "root"
-                    + "&password=" + "root"
-                    + "&autoReconnect=true"
-                    + "&connectTimeout=300";
-
             try {
-                connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/MAS?useUnicode=true&characterEncoding=UTF-8","root","root");
+                connect = DriverManager.getConnection(dbURL, dbUserName, dbPassword);
             } catch (java.sql.SQLException e) {
-                throw new Exception("Can not access to Database Server ..." + url + e.getMessage());
+                throw new Exception("Can not access to Database Server ..." + dbURL + e.getMessage());
             }
         }
         return connect;
