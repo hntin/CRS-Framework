c83ebaf2d9d5197e7a79b1b5e79d76c920bbe723
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-06-29 12:31:57 +0000

    Document Similarity Cosine

--- CRSFramework/src/uit/tkorg/crs/method/content/TFIDF.java
+++ CRSFramework/src/uit/tkorg/crs/method/content/TFIDF.java
@@ -17,38 +17,41 @@ import java.util.HashMap;
  * @author TinHuynh
  */
 public class TFIDF {
-    HashMap<Integer, Integer> AuthorInstanceHM = new HashMap<>();
-    HashMap<Integer, Integer> InstanceAuthorHM = new HashMap<>();
-    private static HashMap<Integer, HashMap<Integer, Float>> _tfidfHM;
+
+    HashMap<Integer, Integer> _AuthorInstanceHM = new HashMap<>();
+    HashMap<Integer, Integer> _InstanceAuthorHM = new HashMap<>();
+    private static HashMap<Integer, HashMap<Integer, Float>> _tfidfHM = new HashMap<>();
+    private HashMap<Integer, String> _InstancePublicationHM = new HashMap<>();
 
     public HashMap<Integer, HashMap<Integer, Float>> process(String inputFile, ArrayList<Integer> listAuthorID) {
         System.out.println("START PROCESSING TFIDF");
+        loadInstancePublication(inputFile);
         try {
             String pathFile = (new File(inputFile)).getParent();
             loadMappingInstanceIDAuthorID(pathFile + "\\CRS-AuthorIDAndInstance.txt");
             for (int inputAuthorID : listAuthorID) {
                 System.out.println("CURRENT INSTANCE IS:" + inputAuthorID);
                 int instanceID = getInstanceFromAuthorID(inputAuthorID);
-                
-                
+
+
             }
-            
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        
+
         System.out.println("FINISH PROCESSING TFIDF");
         return _tfidfHM;
     }
-    
+
     private int getInstanceFromAuthorID(int authorID) {
-        return AuthorInstanceHM.get(authorID);
+        return _AuthorInstanceHM.get(authorID);
     }
-    
+
     private int getAuthorIDFromInstanceID(int instanceID) {
-        return InstanceAuthorHM.get(instanceID);
+        return _InstanceAuthorHM.get(instanceID);
     }
-    
+
     private void loadMappingInstanceIDAuthorID(String mapFile) {
         try {
             FileInputStream fis = new FileInputStream(mapFile);
@@ -65,8 +68,31 @@ public class TFIDF {
 
                 int authorID = Integer.parseInt(tokens[0]);
                 int instanceID = Integer.parseInt(tokens[1]);
-                AuthorInstanceHM.put(authorID, instanceID);
-                InstanceAuthorHM.put(instanceID, authorID);
+                _AuthorInstanceHM.put(authorID, instanceID);
+                _InstanceAuthorHM.put(instanceID, authorID);
+            }
+            bufferReader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void loadInstancePublication(String inputFile) {
+        try {
+            FileInputStream fis = new FileInputStream(inputFile);
+            Reader reader = new InputStreamReader(fis, "UTF8");
+            BufferedReader bufferReader = new BufferedReader(reader);
+            String line = null;
+            String[] tokens;
+            int instanceID = 0;
+            while ((line = bufferReader.readLine()) != null) {
+                tokens = line.split("X\t");
+                if (tokens.length != 2) {
+                    continue;
+                }
+                String publications = tokens[1];
+                _InstancePublicationHM.put(instanceID, publications);
+                instanceID++;
             }
             bufferReader.close();
         } catch (Exception ex) {
