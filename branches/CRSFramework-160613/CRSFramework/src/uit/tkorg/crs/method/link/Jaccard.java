939828c97da93d1fe89d4ef6f4bdf7e8d9b55692
tin.huynh.uit@gmail.com <tin.huynh.uit@gmail.com@585e8c69-b0d5-62e4-9894-778d353b6c62>
2013-06-29 03:51:24 +0000

    Document Similarity Cosine

--- branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/link/Jaccard.java
+++ branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/link/Jaccard.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package uit.tkorg.crs.method;
+package uit.tkorg.crs.method.link;
 
 import java.util.ArrayList;
 import java.util.HashMap;
