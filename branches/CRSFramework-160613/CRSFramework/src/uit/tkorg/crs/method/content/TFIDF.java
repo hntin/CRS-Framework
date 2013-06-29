15d9996b13605dbf0c3d5b8fb1c6e5340994a217
tin.huynh.uit@gmail.com <tin.huynh.uit@gmail.com@585e8c69-b0d5-62e4-9894-778d353b6c62>
2013-06-29 11:38:47 +0000

    Document Similarity Cosine

--- branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/content/TFIDF.java
+++ branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/content/TFIDF.java
@@ -4,10 +4,28 @@
  */
 package uit.tkorg.crs.method.content;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author TinHuynh
  */
 public class TFIDF {
-    
+    HashMap<Integer, Integer> AuthorInstanceHM = new HashMap<>();
+    HashMap<Integer, Integer> InstanceAuthorHM = new HashMap<>();
+    private static HashMap<Integer, HashMap<Integer, Float>> _tfidfHM;
+
+    public HashMap<Integer, HashMap<Integer, Float>> process(String inputFile, ArrayList<Integer> listAuthorID) {
+        System.out.println("START PROCESSING TFIDF");
+        try {
+            
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        System.out.println("FINISH PROCESSING TFIDF");
+        return _tfidfHM;
+    }
 }
