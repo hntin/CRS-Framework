f24514e69721f9fe7b4e00db4a7f1ee21986e88c
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-06-29 03:16:07 +0000

    Document Similarity Cosine

---
+++ CRSFramework/src/uit/tkorg/crs/method/TFIDF.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.crs.method;
+
+/**
+ *
+ * @author TinHuynh
+ */
+public class TFIDF {
+    
+}
