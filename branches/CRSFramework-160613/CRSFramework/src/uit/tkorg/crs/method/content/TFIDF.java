55099510d9933c7fab99abe72eeba92e9cb065ac
tin.huynh.uit@gmail.com <tin.huynh.uit@gmail.com@585e8c69-b0d5-62e4-9894-778d353b6c62>
2013-06-29 12:01:35 +0000

    Document Similarity Cosine

--- branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/content/TFIDF.java
+++ branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/content/TFIDF.java
@@ -4,6 +4,10 @@
  */
 package uit.tkorg.crs.method.content;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -27,5 +31,38 @@ public class TFIDF {
         
         System.out.println("FINISH PROCESSING TFIDF");
         return _tfidfHM;
+    }
+    
+    private int getInstanceFromAuthorID(int authorID) {
+        return AuthorInstanceHM.get(authorID);
+    }
+    
+    private int getAuthorIDFromInstanceID(int instanceID) {
+        return InstanceAuthorHM.get(instanceID);
+    }
+    
+    private void loadMappingInstanceIDAuthorID(String mapFile) {
+        try {
+            FileInputStream fis = new FileInputStream(mapFile);
+            Reader reader = new InputStreamReader(fis, "UTF8");
+            BufferedReader bufferReader = new BufferedReader(reader);
+            bufferReader.readLine(); // skip the header line
+            String line = null;
+            String[] tokens;
+            while ((line = bufferReader.readLine()) != null) {
+                tokens = line.split("\t");
+                if (tokens.length != 2) {
+                    continue;
+                }
+
+                int authorID = Integer.parseInt(tokens[0]);
+                int instanceID = Integer.parseInt(tokens[1]);
+                AuthorInstanceHM.put(authorID, instanceID);
+                InstanceAuthorHM.put(instanceID, authorID);
+            }
+            bufferReader.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
