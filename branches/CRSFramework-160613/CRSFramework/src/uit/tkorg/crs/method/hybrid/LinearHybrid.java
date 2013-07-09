43f562556f3fe8a76a1b860b665130e4b5213842
tin.huynh.uit@gmail.com <tin.huynh.uit@gmail.com@585e8c69-b0d5-62e4-9894-778d353b6c62>
2013-07-09 02:07:26 +0000

    Implement Linear Hybrid Method

--- branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/hybrid/LinearHybrid.java
+++ branches/CRSFramework-160613/CRSFramework/src/uit/tkorg/crs/method/hybrid/LinearHybrid.java
@@ -11,31 +11,38 @@ import java.util.HashMap;
  * @author TinHuynh
  */
 public class LinearHybrid {
-    private float k = (float) 0.5;
 
+    private float k = (float) 0.5;
     HashMap<Integer, HashMap<Integer, Float>> linearHybridResult = null;
-    public  HashMap<Integer, HashMap<Integer, Float>> calculatingLinearHybriđ(
-                                    HashMap<Integer, HashMap<Integer, Float>> rssResult, 
-                                    HashMap<Integer, HashMap<Integer, Float>> tfidfResult) {
-        
+
+    public HashMap<Integer, HashMap<Integer, Float>> calculatingLinearHybriđ(
+            HashMap<Integer, HashMap<Integer, Float>> rssResult,
+            HashMap<Integer, HashMap<Integer, Float>> tfidfResult) {
+
         linearHybridResult = new HashMap<>();
-        for (int authorID1 :  rssResult.keySet()) {
-             for (int authorID2 : rssResult.get(authorID1).keySet()) {
-                 float linkValue = rssResult.get(authorID1).get(authorID2);
-                 float contentValue = tfidfResult.get(authorID1).get(authorID2);
-                 float hybridValue = (float) k*linkValue + (float)(1-k)*contentValue;
-                 
-                 HashMap<Integer, Float> hybridStrengthHM = linearHybridResult.get(authorID1);
-                 if (hybridStrengthHM == null) {
-                     hybridStrengthHM =  new HashMap();
-                 }
-                 
-                 hybridStrengthHM.put(authorID2, hybridValue);
-                 linearHybridResult.put(authorID1, hybridStrengthHM);
-             }
+        for (int authorID1 : tfidfResult.keySet()) {
+            for (int authorID2 : (tfidfResult.get(authorID1)).keySet()) {
+                if (authorID2 != authorID1) {
+                    float linkValue = 0;
+                    float contentValue = 0;
+                    if (rssResult.containsKey(authorID1) && rssResult.get(authorID1).containsKey(authorID2)) {
+                        linkValue = rssResult.get(authorID1).get(authorID2);
+                    }
+
+                    contentValue = tfidfResult.get(authorID1).get(authorID2);
+                    float hybridValue = (float) k * linkValue + (float) (1 - k) * contentValue;
+
+                    HashMap<Integer, Float> hybridStrengthHM = linearHybridResult.get(authorID1);
+                    if (hybridStrengthHM == null) {
+                        hybridStrengthHM = new HashMap();
+                    }
+
+                    hybridStrengthHM.put(authorID2, hybridValue);
+                    linearHybridResult.put(authorID1, hybridStrengthHM);
+                }
+            }
         }
-        
+
         return linearHybridResult;
     }
-            
 }
