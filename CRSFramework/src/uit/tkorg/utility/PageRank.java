f97b804109cfebbaa875d510c4f44f3e966c1719
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-07-14 09:55:57 +0000

    fix bug: KLDivergence calculation

---
+++ CRSFramework/src/uit/tkorg/utility/PageRank.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.utility;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author TinHuynh
+ */
+public class PageRank {
+    
+    /**
+     * 
+     * @return HashMap<NodeID, ImportantRate> 
+     */
+    public HashMap<Integer, Float> calculatePR() {
+        
+        return null;
+    }
+}
