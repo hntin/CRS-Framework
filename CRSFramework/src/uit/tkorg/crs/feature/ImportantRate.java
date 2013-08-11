26cfda5496de06ef03ce1e50136208dfa080a531
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-08-11 09:09:47 +0000

    Start coding for the Isolated Author Recommendation

---
+++ CRSFramework/src/uit/tkorg/crs/feature/ImportantRate.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.crs.feature;
+
+/**
+ *
+ * @author TinHuynh
+ */
+public class ImportantRate {
+    
+}
