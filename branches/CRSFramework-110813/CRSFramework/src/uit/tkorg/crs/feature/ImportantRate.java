7203b925f96610dd7d76b8c0301c1b4d973c1776
tin.huynh.uit@gmail.com <tin.huynh.uit@gmail.com@585e8c69-b0d5-62e4-9894-778d353b6c62>
2013-08-17 08:53:42 +0000

    Building the Citation Graph, Reference Graph

--- branches/CRSFramework-110813/CRSFramework/src/uit/tkorg/crs/feature/ImportantRate.java
+++ branches/CRSFramework-110813/CRSFramework/src/uit/tkorg/crs/feature/ImportantRate.java
@@ -4,10 +4,31 @@
  */
 package uit.tkorg.crs.feature;
 
+import java.util.HashMap;
+import uit.tkorg.crs.common.PageRank;
+import uit.tkorg.crs.graph.CitationGraph;
+import uit.tkorg.utility.TextFileUtility;
+
 /**
  *
  * @author TinHuynh
  */
 public class ImportantRate {
+
+    public void processImportantRate() {
+        try {
+            System.out.println("START...");
+            CitationGraph citedGraph = new CitationGraph(
+                    "C:\\CRS-Experiment\\MAS\\Input\\Input2\\[TrainingData]AuthorID_PaperID_All.txt",
+                    "C:\\CRS-Experiment\\MAS\\Input\\Input2\\[TrainingData]PaperID_Year_ReferenceID_1995_2005.txt");
+            citedGraph.calculateImportantRate("C:\\CRS-Experiment\\MAS\\Output\\pagerank.txt");
+            System.out.println("END...");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
     
+    public static void main(String args[]){
+        (new ImportantRate()).processImportantRate();
+    }
 }
