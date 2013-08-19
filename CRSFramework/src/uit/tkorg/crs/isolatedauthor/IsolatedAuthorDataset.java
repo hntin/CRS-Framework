a0f0da2feaef5239cb8de63cae376f3bdf3a8dbb
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-08-19 09:34:50 +0000

    Implement ActiveScore

---
+++ CRSFramework/src/uit/tkorg/crs/isolatedauthor/IsolatedAuthorDataset.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.crs.isolatedauthor;
+
+/**
+ *
+ * @author TinHuynh
+ */
+public class IsolatedAuthorDataset {
+    
+    private void load_Author_Information() {
+        
+    }
+    
+    private void extractIsolatedAuthorInfo() {
+        
+    }
+    
+
+}
