b78c42c43a1c4e6654b3f2b2533a7b2dc9d36f53
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-08-26 02:27:38 +0000

    Building true pairs and false pairs

---
+++ CRSFramework/src/uit/tkorg/crs/isolatedauthor/feature/FeatureObj.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.crs.isolatedauthor.feature;
+
+/**
+ *
+ * @author TinHuynh
+ */
+public class FeatureObj {
+    String _name;
+    float _weight;  
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String _name) {
+        this._name = _name;
+    }
+
+    public float getWeight() {
+        return _weight;
+    }
+
+    public void setWeight(float _weight) {
+        this._weight = _weight;
+    }
+}
