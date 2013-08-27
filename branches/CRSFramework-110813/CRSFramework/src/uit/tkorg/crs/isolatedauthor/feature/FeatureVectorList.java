fecd6954f74a56153e72f9df62d8f2ed83532179
tin.huynh.uit@gmail.com <tin.huynh.uit@gmail.com@585e8c69-b0d5-62e4-9894-778d353b6c62>
2013-08-27 13:57:27 +0000

    Loading Data for training and testing from XML files

--- branches/CRSFramework-110813/CRSFramework/src/uit/tkorg/crs/isolatedauthor/feature/FeatureVectorList.java
+++ branches/CRSFramework-110813/CRSFramework/src/uit/tkorg/crs/isolatedauthor/feature/FeatureVectorList.java
@@ -1,16 +1,20 @@
 package uit.tkorg.crs.isolatedauthor.feature;
 
+import java.awt.BorderLayout;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.Node;
+import uit.tkorg.utility.TextFileUtility;
 import uit.tkorg.utility.XMLFileUtility;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.ThresholdCurve;
 import weka.classifiers.trees.J48;
 import weka.classifiers.functions.LibSVM;
 import weka.classifiers.lazy.IBk;
@@ -20,6 +24,9 @@ import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SparseInstance;
+import weka.core.Utils;
+import weka.gui.visualize.PlotData2D;
+import weka.gui.visualize.ThresholdVisualizePanel;
 
 /**
  *
@@ -53,7 +60,7 @@ public class FeatureVectorList {
                 featureVectorObject.setContentSimValue(contentSimValue);
                 featureVectorObject.setOrgRSSValue(orgRSSValue);
                 featureVectorObject.setImportantRateValue(importantRateValue);
-                featureVectorObject.setActiveScoreValue(activeScoreValue);
+//                featureVectorObject.setActiveScoreValue(activeScoreValue);
 
                 Element labelNode = pairElement.element("tag");
                 if (labelNode.getText().equalsIgnoreCase("1")) {
@@ -72,7 +79,6 @@ public class FeatureVectorList {
     }
 
     public static Instances formatFeatureVectorAsInstances(List<FeatureVectorObject> listOfVectors, int numberOfVector) {
-        // Xây dựng să4n cấu trúc của các vector gồm bao nhiêu chiều có tên gì thuộc tính tên gi
         Instances instancesData = formatFeatureVectorAsWekaStructure(FeatureVectorObject.NUMBER_OF_FEATURE, numberOfVector);
         for (int i = 0; i < numberOfVector; i++) {
             // Insert values of features for each vector 
@@ -110,9 +116,6 @@ public class FeatureVectorList {
 
     public static Instance insertFeatureValue(Instances instancesData, FeatureVectorObject featureVector, int dimension) {
         Instance oneInstance = new SparseInstance(dimension);
-
-        // Add các giá trị của các đặc trưng vào ( các giá trị theo các chiều trong vector) 
-        // Bao gồm cả thuộc tính gán nhãn của vector.
         oneInstance.setValue((Attribute) instancesData.attribute(FeatureVectorObject.CONTENT_SIM), featureVector.contentSimValue);
         oneInstance.setValue((Attribute) instancesData.attribute(FeatureVectorObject.ORGANIZATION_RSS), featureVector.orgRSSValue);
         oneInstance.setValue((Attribute) instancesData.attribute(FeatureVectorObject.IMPORTANT_RATE), featureVector.importantRateValue);
@@ -131,77 +134,33 @@ public class FeatureVectorList {
             FeatureVectorList temp = new FeatureVectorList();
             ArrayList<FeatureVectorObject> featureVectorList1 = temp.buildingFeatureVectorListFromXMLFile(
                     "C:\\CRS-Experiment\\MAS\\ColdStart\\Input\\Input1\\TruePair1.xml");
-
             ArrayList<FeatureVectorObject> featureVectorList2 = temp.buildingFeatureVectorListFromXMLFile(
                     "C:\\CRS-Experiment\\MAS\\ColdStart\\Input\\Input1\\FalsePair1.xml");
-
             ArrayList<FeatureVectorObject> featureVectorList = new ArrayList<>();
             featureVectorList.addAll(featureVectorList1);
             featureVectorList.addAll(featureVectorList2);
 
-            // Tach featureVectorList thanh tap train va tap test 
-            int sizeOfTrainingSet = (int) 60 * featureVectorList.size() / 100;
-            int sizeOfTestingSet = featureVectorList.size() - sizeOfTrainingSet;
-            ArrayList<FeatureVectorObject> trainingSet = new ArrayList<>();
-            ArrayList<FeatureVectorObject> testingSet = new ArrayList<>();
+            Instances data = temp.formatFeatureVectorAsInstances(featureVectorList, featureVectorList.size());
+            data.setClassIndex(data.numAttributes() - 1);
 
-            int numOfTruePairInTrainingSet = sizeOfTrainingSet / 2;
-            int numOfFalsePairInTrainingSet = sizeOfTrainingSet - numOfTruePairInTrainingSet;
+            // train classifier
+            Classifier cslSVM = new LibSVM();
+            Evaluation eval = new Evaluation(data);
+            eval.crossValidateModel(cslSVM, data, 10, new Random(1));
 
-            // Duyet qua cac truepair
-            for (int i = 0; i < featureVectorList1.size(); i++) {
-                if (i <= numOfTruePairInTrainingSet) {
-                    trainingSet.add(featureVectorList1.get(i));
-                } else {
-                    testingSet.add(featureVectorList1.get(i));
-                }
-            }
-
-            for (int i = 0; i < featureVectorList2.size(); i++) {
-                if (i <= numOfFalsePairInTrainingSet) {
-                    trainingSet.add(featureVectorList2.get(i));
-                } else {
-                    testingSet.add(featureVectorList2.get(i));
-                }
-            }
-
-            // Initial Instance for the training & testing set
-            System.out.println("TrainingSet size:" + trainingSet.size());
-            System.out.println("TestingSet size:" + testingSet.size());
-            Instances train = temp.formatFeatureVectorAsInstances(trainingSet, trainingSet.size()); // Train Data
-            Instances test = temp.formatFeatureVectorAsInstances(testingSet, testingSet.size()); // Testing Data
-
-            // Call Weka API to build classifier and evaluate
-            // Using LibSVM
-            Classifier clsSVM = new LibSVM();
-            clsSVM.buildClassifier(train);
-            Evaluation eval = new Evaluation(train);
-            eval.evaluateModel(clsSVM, test);
-            System.out.println(eval.toSummaryString("\nResults Using SVM \n======\n", false));
+            StringBuffer strBuff = new StringBuffer();
+            strBuff.append("Feature" + "\t" + "Class" + "\t" + "Correctly Classified Instances" + "\t" + 
+                    "Incorrectly Classified Instances" + "\t" + "Precision" + "\t" + "Recall" + "\t" + "F-Measure" + "\n");
+            strBuff.append("ContentSim & OrgRSS & I.Rate & A.Score" + "\t" + "HasLink (Yes)" + "\t" + eval.pctCorrect() + "\t" + eval.pctIncorrect() + "\t" + eval.precision(0)*100 + "\t" + eval.recall(0)*100 + "\t" + eval.fMeasure(0)*100 + "\n");
+            strBuff.append("" + "\t" + "HasLink (No)" + "\t" + eval.pctCorrect() + "\t" + eval.pctIncorrect() + "\t" + eval.precision(1)*100 + "\t" + eval.recall(1)*100 + "\t" + eval.fMeasure(1)*100 + "\n");
+            strBuff.append("" + "\t" + "Weighted Average" + "\t" + eval.pctCorrect() + "\t" + eval.pctIncorrect() + "\t" + eval.weightedPrecision()*100 + "\t" + eval.weightedRecall()*100 + "\t" + eval.weightedFMeasure()*100 + "\n");
             
-            // Using RandomForest
-            Classifier clsRF = new RandomForest();
-            clsRF.buildClassifier(train);
-            eval.evaluateModel(clsRF, test);
-            System.out.println(eval.toSummaryString("\nResults Using Random Forest \n======\n", false));
-            
-            // Using NaiveBayes
-            Classifier clsNB = new RandomForest();
-            clsNB.buildClassifier(train);
-            eval.evaluateModel(clsNB, test);
-            System.out.println(eval.toSummaryString("\nResults Using Random NaiveBayes \n======\n", false));
-            
-            // Using KNN
-            Classifier clsKNN = new IBk();
-            clsKNN.buildClassifier(train);
-            eval.evaluateModel(clsKNN, test);
-            System.out.println(eval.toSummaryString("\nResults Using KNN \n======\n", false));
-            
-            // Using C45            
-            Classifier clsC45 = new J48();
-            clsC45.buildClassifier(train);
-            eval.evaluateModel(clsC45, test);
             System.out.println(eval.toSummaryString("\nResults Using C45 \n======\n", false));
+            System.out.println(eval.weightedPrecision());
+            System.out.println(eval.weightedRecall());
+            System.out.println(eval.weightedFMeasure());
+            TextFileUtility.writeTextFile("C:\\CRS-Experiment\\MAS\\ColdStart\\Output\\4_Features_Result.txt", strBuff.toString());
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
