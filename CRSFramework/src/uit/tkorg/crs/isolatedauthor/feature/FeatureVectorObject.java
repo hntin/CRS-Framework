e6a0344c2441cf880f137bbaf2ad5bdd3c655009
tin.huynh.uit <tin.huynh.uit@gmail.com>
2013-09-16 04:22:55 +0000

    Create a new branch

---
+++ CRSFramework/src/uit/tkorg/crs/isolatedauthor/feature/FeatureVectorObject.java
@@ -0,0 +1,61 @@
+package uit.tkorg.crs.isolatedauthor.feature;
+
+/**
+ *
+ * @author TinHuynh
+ */
+public class FeatureVectorObject {
+    public static final int NUMBER_OF_FEATURE = 1;
+    public static final String CONTENT_SIM = "contentSim";
+    float contentSimValue;
+    public static final String ORGANIZATION_RSS = "orgRSS";
+    float orgRSSValue;    
+    public static final String IMPORTANT_RATE = "importantRate";
+    float importantRateValue;  
+    public static final String ACTIVE_SCORE = "activeScore";
+    float activeScoreValue;
+    
+    public static final String LABEL_CLASS = "label";
+    public String labelValue;
+
+    public float getContentSimValue() {
+        return contentSimValue;
+    }
+
+    public void setContentSimValue(float contentSimValue) {
+        this.contentSimValue = contentSimValue;
+    }
+
+    public float getOrgRSSValue() {
+        return orgRSSValue;
+    }
+
+    public void setOrgRSSValue(float orgRSSValue) {
+        this.orgRSSValue = orgRSSValue;
+    }
+
+    public float getImportantRateValue() {
+        return importantRateValue;
+    }
+
+    public void setImportantRateValue(float importantRateValue) {
+        this.importantRateValue = importantRateValue;
+    }
+
+    public float getActiveScoreValue() {
+        return activeScoreValue;
+    }
+
+    public void setActiveScoreValue(float activeScoreValue) {
+        this.activeScoreValue = activeScoreValue;
+    }
+
+    public String getLabelValue() {
+        return labelValue;
+    }
+
+    public void setLabelValue(String labelValue) {
+        this.labelValue = labelValue;
+    }
+
+}
---
+++ CRSFramework/src/uit/tkorg/crs/method/link/RandomPrediction.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uit.tkorg.crs.method.link;
+
+/**
+ *
+ * @author TinHuynh
+ */
+public class RandomPrediction {
+    
+}
