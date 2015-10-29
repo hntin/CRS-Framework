79c9db462b8e6f7474b88beb62d19a8590fcf608
TinHuynh <TinHuynh@192.168.10.100>
2015-10-29 09:34:29 +0700

    prepare training data with many features

--- src/main/java/uit/tkorg/crs/datapreparation/CoAuthorStrengthComputation.java
+++ src/main/java/uit/tkorg/crs/datapreparation/CoAuthorStrengthComputation.java
@@ -6,9 +6,18 @@
 package uit.tkorg.crs.datapreparation;
 
 /**
- *
+ * Tinh CoAuthorStrength (RSS+) cho tung cap tac gia trong mau am (-) va mau duong (+)
  * @author thucnt
  */
 public class CoAuthorStrengthComputation {
+    
+    public static void main(String args[]) {
+        // Step 1: Xay dung mang dong tac gia CoAuthor_Net
+        // Step 2: Tinh toan trong so RSS cho tung cap tac gia trong mang
+        // Step 3: Doc file mau am (-), mau duong (+) de load tung cap <authorID, authorID>
+        // Step 4: Tra ve gia tri RSS+ cho tung cap (+) va (-)
+        // Step 5: Ghi ket qua gia tri CoAuthor_RSS xuong file 
+        
+    }
     
 }
--- src/main/java/uit/tkorg/crs/datapreparation/CoOrgStrengthComputation.java
+++ src/main/java/uit/tkorg/crs/datapreparation/CoOrgStrengthComputation.java
@@ -11,4 +11,13 @@ package uit.tkorg.crs.datapreparation;
  */
 public class CoOrgStrengthComputation {
     
+    public static void main(String args[]) {
+        // Step 1: Xay dung mang CoOrg_Net.
+        // Step 2: Tinh toan trong so RSS cho tung cap OrgID trong mang.
+        // Step 3: Doc file mau am (-), mau duong (+) de load tung cap <authorID, authorID>.
+        // Step 4: Lay OrgID cua 2 tac gia tuong ung.
+        // Step 5: Tra ve gia tri RSS+ cho tung cap OrgID lien quan cac cap authorID trong mau (+) va (-)
+        // Step 6: Ghi ket qua gia tri Org_RSS xuong file 
+        
+    }
 }
