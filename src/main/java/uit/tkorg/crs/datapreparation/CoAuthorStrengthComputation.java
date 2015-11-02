d3c8ccc28c8925ed749d9382249b3003134d870e
TinHuynh <TinHuynh@192.168.10.100>
2015-11-02 08:44:30 +0700

    prepare training data with many features

--- src/main/java/uit/tkorg/crs/datapreparation/CoAuthorStrengthComputation.java
+++ src/main/java/uit/tkorg/crs/datapreparation/CoAuthorStrengthComputation.java
@@ -14,53 +14,26 @@ import java.util.regex.Pattern;
 import org.apache.mahout.common.Pair;
 
 /**
- * Tinh CoAuthorStrength (RSS+) cho tung cap tac gia trong mau am (-) va mau duong (+)
+ * Tinh CoAuthorStrength (RSS+) cho tung cap tac gia trong mau am (-) va mau
+ * duong (+)
+ *
  * @author thucnt
  */
-public class CoAuthorStrengthComputation {
-    
-    /**
-     * read positive/negative samples from file
-     * @param dataFile
-     */
-    public static ArrayList<Pair<Integer,Integer>> readSample(String dataFile){
-        final String REGEX = "\\D";
-        Pattern p = Pattern.compile(REGEX);
-        ArrayList<Pair<Integer,Integer>> listOfPairs = new ArrayList<Pair<Integer,Integer>>();
-        
-        try {
-            FileInputStream fis = new FileInputStream(dataFile);
-            Reader reader = new InputStreamReader(fis, "UTF8");
-            BufferedReader bufferReader = new BufferedReader(reader);
-            bufferReader.readLine(); // skip the first line
-            String line = null;
-            
-            while ((line = bufferReader.readLine()) != null) {
-                String[] elements = p.split(line.trim());
+public class CoAuthorStrengthComputation extends FeatureComputation {
 
-                if (elements.length > 3 || elements.length < 2) {
-                    continue;
-                }
-                int author1 = Integer.parseInt(elements[1]);
-                int author2 = Integer.parseInt(elements[2]);
-                Pair pair = new Pair(new Integer(author1),new Integer(author2));
-                listOfPairs.add(pair);
-            }
-            bufferReader.close();
-            fis.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return listOfPairs;
-    }
-    
-    public static void main(String args[]) {
+    @Override
+    public void computeFeatureValues(String outputFile) {
         // Step 1: Xay dung mang dong tac gia CoAuthor_Net
         // Step 2: Tinh toan trong so RSS cho tung cap tac gia trong mang
         // Step 3: Doc file mau am (-), mau duong (+) de load tung cap <authorID, authorID>
         // Step 4: Tra ve gia tri RSS+ cho tung cap (+) va (-)
         // Step 5: Ghi ket qua gia tri CoAuthor_RSS xuong file 
         
+        
     }
-    
+
+    public static void main(String args[]) {
+
+    }
+
 }
--- src/main/java/uit/tkorg/crs/datapreparation/CoOrgStrengthComputation.java
+++ src/main/java/uit/tkorg/crs/datapreparation/CoOrgStrengthComputation.java
@@ -11,20 +11,20 @@ package uit.tkorg.crs.datapreparation;
  *
  * @author thucnt
  */
-public class CoOrgStrengthComputation extends FeatureComputattion {
+public class CoOrgStrengthComputation extends FeatureComputation {
 
     @Override
     public void computeFeatureValues(String outputFile) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public static void main(String args[]) {
         // Step 1: Xay dung mang CoOrg_Net.
         // Step 2: Tinh toan trong so RSS cho tung cap OrgID trong mang.
         // Step 3: Doc file mau am (-), mau duong (+) de load tung cap <authorID, authorID>.
         // Step 4: Lay OrgID cua 2 tac gia tuong ung.
         // Step 5: Tra ve gia tri RSS+ cho tung cap OrgID lien quan cac cap authorID trong mau (+) va (-)
         // Step 6: Ghi ket qua gia tri Org_RSS xuong file 
+    }
+
+    public static void main(String args[]) {
+        
 
     }
 }
