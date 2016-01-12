fc6280d2c245035a6cfd5eed819e3cc35f4bb34e
TinHuynh <TinHuynh@192.168.10.100>
2016-01-12 14:59:51 +0700

    prepare training data with many features

--- src/main/java/uit/tkorg/crs/experiment/CollaborativeQualityEvaluation.java
+++ src/main/java/uit/tkorg/crs/experiment/CollaborativeQualityEvaluation.java
@@ -1,5 +1,4 @@
-package uit.tkorg.crs.utility;
-
+package uit.tkorg.crs.experiment;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -8,19 +7,31 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Set;
 import uit.tkorg.crs.isolatedauthor.IsolatedAuthorDataset;
+import uit.tkorg.crs.model.CoAuthorGraph;
 import uit.tkorg.crs.utility.HashMapUtility;
+import uit.tkorg.crs.utility.HashMapUtility;
+import uit.tkorg.crs.utility.TextFileUtility;
 import uit.tkorg.crs.utility.TextFileUtility;
 
 /**
  *
  * @author TinHuynh
  */
-public class GoodnessEvaluation {
+public class CollaborativeQualityEvaluation {
 
     public HashMap<Integer, Float> decisionValHM = new HashMap<>(); // <InstanceID, DecisionValue>
     public HashMap<Integer, Integer[]> instanceID_Pair_HM = new HashMap<>();
+    public CoAuthorGraph _graph;
+    
+    public CollaborativeQualityEvaluation() {
+    }
+    
+    public CollaborativeQualityEvaluation(String file_AuthorID_PaperID, String file_PaperID_Year, int startYear, int endYear) {
+        _graph = new CoAuthorGraph(file_AuthorID_PaperID, file_PaperID_Year, startYear, endYear);
+    }
 
     /**
      * return: decisionValHM
@@ -156,28 +167,35 @@ public class GoodnessEvaluation {
     }
 
     /**
-     * Getting goodness of coauthor-ship based on period of collaboration
-     *
-     * @param topN
-     * @return: goodness metric 2
+     * 
+     * @param authorID
+     * @param potentialCoAuthorList
+     * @param file_AuthorID_PaperID
+     * @param file_PaperID_Year
+     * @param startYear
+     * @param endYear
+     * @return 
      */
-    public float getCoAuthorshipGoodness_Metric2(int topN) {
-        float goodnessVal = 0.f;
-
-        return goodnessVal;
-    }
-
-    /**
-     * Getting goodness of coauthor-ship based on ability to extend new
-     * coauthor-ship
-     *
-     * @param topN
-     * @return: goodness metric 2
-     */
-    public float getCoAuthorshipGoodness_Metric3(int topN) {
-        float goodnessVal = 0.f;
-
-        return goodnessVal;
+    public double calculate_CollaborativeQualityTopN_Metric1(String authorID, List<String> potentialCoAuthorList_TopN, 
+            String file_AuthorID_PaperID, String file_PaperID_Year, int startYear, int endYear) {
+        
+        double collaborativeQualityTopN = 0;        
+        // i la vi tri xep hang cua potentialCoAuthorID trong danh sach?
+        for (int i=0; i<potentialCoAuthorList_TopN.size(); i++) {
+            String potentialCoAuthorID = potentialCoAuthorList_TopN.get(i);          
+            // Kiem tra su ton tai cua authorID va potentialCoAuthorID trong T3
+            // Dem so bai dong tac gia cua (authorID, potentialCoAuthorID) trong T3. 
+            // Tinh chat luong cong tac dua tren so bai dong tac gia cua (authorID, potentialCoAuthorID)
+            // Cong don vao ket qua Quality_Top_N(r(i))
+            if (_graph._coAuthorGraph.containsKey(authorID) && _graph._coAuthorGraph.get(authorID).containsKey(potentialCoAuthorID)) {
+                int numOfCollaboration = _graph._coAuthorGraph.get(authorID).get(potentialCoAuthorID);
+                collaborativeQualityTopN += numOfCollaboration*1/(Math.exp(i));
+            }
+        }
+        
+        // Average value
+        collaborativeQualityTopN = collaborativeQualityTopN/potentialCoAuthorList_TopN.size();        
+        return collaborativeQualityTopN;        
     }
 
     public static void main(String args[]) {
@@ -194,7 +212,7 @@ public class GoodnessEvaluation {
         isolatedDataset.build_NF_FF_Graph();
         isolatedDataset.build_CoAuthorGraph();
 
-        GoodnessEvaluation goodnessEvaluation = new GoodnessEvaluation();
+        CollaborativeQualityEvaluation goodnessEvaluation = new CollaborativeQualityEvaluation();
         goodnessEvaluation.load_Decsion_Value("C:\\CRS-Experiment\\MAS\\ColdStart\\Output\\TestDataset_2Features_OrgRS_ActiveScore_results.txt");
 
         goodnessEvaluation.load_InstanceID_Pair("C:\\CRS-Experiment\\MAS\\ColdStart\\Input\\Input1\\TestDatasetMapping.txt");
@@ -207,3 +225,30 @@ public class GoodnessEvaluation {
         System.out.println("END");
     }
 }
+
+/**
+ * Input: List of researchers, TopN
+ * Output: Quality of Recommendation for inputted researchers = Sum(Quality_TopN(r(i)))/Total_Input_Researchers
+ * Step 1: ForAll r(i)
+ * Step 2: Begin
+ *          Doc file Output cua Model?, lay TopN nguoi cong tac voi r(i) co xac xuat cao nhat
+ *          (Xay dung lai file Output cua Model: (AuthorID, CoAuthorID, Prob, True/False))
+ *          Tinh Quality_TopN(r(i), TopN_Potential_List)
+ * Step 3: End
+ * Step 4: Tinh Average_Quality_TopN cho tat ca cac input researchers = Sum(Quality_TopN(r(i)))/Total_Input_Researchers
+ * Step 5: Output ra file?
+ */
+
+/**
+ * Tinh Metric 1: Quality_Top_N(r(i))
+ * Input: r(i), TopN_Potential_List
+ * Output: Gia tri Quality_Top_N(r(i))
+ * Step 1: Build G3 tuong ung voi T3
+ * Step 2: ForAll Coauthor(j) in TopN_Potential_List
+ * Step 3:  Kiem tra su ton tai cua r(i) va CoAuthor(j) trong T3
+ *          Dem so bai bao dong tac gia r(i) va CoAuthor(j) in T3.
+ * Step 4:  Tinh theo cong thuc Quality dua tren vi tri cua j trong TopN.
+ *          Cong don vao ket qua Quality_Top_N(r(i))
+ * Step 5: End 
+ * Step 6: Return ket qua
+ */
